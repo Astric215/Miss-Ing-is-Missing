775762d8ad7649835fdc8fd9d27a2a3be13f0b48
nameA42 <bepratt@ucsc.edu>
2022-05-11 10:37:58 -0700

    menuButton

--- src/Scenes/Menu.js
+++ src/Scenes/Menu.js
@@ -8,24 +8,27 @@ class Menu extends Phaser.Scene
         super("menuScene");
     }
 
-    create()
-    {
-    }
-
     create() 
     {
-
+        this.menubutton = this.add.rectangle(200, 200, 100, 100, 0xff0000);
+        
+        //check if the left button is down on the mouse if so go to play
+        //click to start
+        this.input.on('gameobjectdown', (pointer, gameObject) =>
+            {
+                console.log("pogo");
+                if(gameObject == this.menubutton)
+                {
+                    this.scene.start("CharacterSelectionScene");
+                }
+            },
+            this
+        );
     }
 
     update() 
     {
         console.log('menuscene');
         
-        //check if the left button is down on the mouse if so go to play
-        //click to start
-        if(this.input.activePointer.leftButtonDown())
-        {
-            this.scene.start("CharacterSelectionScene");
-        }
     }
 }
