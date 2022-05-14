a7cd9f819b1643c57988a889aa6bbbad406bd959
Astric215 <davidiwd215@gmail.com>
2022-05-14 02:15:03 -0700

    basic bfs set up but i need adjacency lists

--- src/Objects/Pathfinder.js
+++ src/Objects/Pathfinder.js
@@ -1,7 +1,30 @@
 class Pathfinder
 {
-   constructor(scene)
-   {
-   
-   }
+    constructor(pip, start)
+    { 
+        
+    }
+
+    //basic pathfinding via bfs: bfs(start) where start is a tile
+    bfs(start)
+    {
+       //start is a tile from the map. The current tile the player is on
+       let frontier = [];
+       frontier.push(this.start);
+       let reached = new Set();
+       reached.add(this.start);
+
+        while(frontier.size != 0)
+        {
+            let current = frontier.pop()
+            for(let i = 0; i < current.adjacent.length; i++)
+            {
+                if(!reached.has(current.adjacent[i])){
+                    frontier.push(current.adjacent[i]);
+                    reached.add(current.adjacent[i]);
+                }
+
+            }
+        }           
+    }
 }
--- src/Objects/Tile.js
+++ src/Objects/Tile.js
@@ -5,6 +5,7 @@ class Tile extends Phaser.GameObjects.Image
         super(scene, x, y, texture, frame, options);
         scene.add.existing(this);
         this.config = config;
+        this.adjacent = [];
     }
 
     update()
