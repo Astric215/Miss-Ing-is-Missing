c7bfa2686ef871afcfe89510c76e12c147973bad
Newt Novak <manovak@ucsc.edu>
2022-05-15 20:58:46 -0700

    clothing preload

--- src/Scenes/Load.js
+++ src/Scenes/Load.js
@@ -14,6 +14,9 @@ class Load extends Phaser.Scene
         this.load.atlas('tileAtlas', './assets/Atlases/Tiles.png', './assets/Atlases/Tiles.json');
         this.load.atlas('tPipSprites', './assets/Atlases/tmp/tmpPip.png', './assets/Atlases/tmp/tmpPip.json');
         this.load.atlas('bodyAtlas', './assets/Atlases/tmp/bodySheet.png', './assets/Atlases/tmp/bodySheet.json');
+        this.load.atlas('ladyClothingAtlas', './assets/Atlases/tmp/ladyClothing.png', './assets/Atlases/tmp/ladyClothing.json');
+        this.load.atlas('dudeClothingAtlas', './assets/Atlases/tmp/dudeClothing.png', './assets/Atlases/tmp/dudeClothing.json');
+
         
         //load images
         this.load.image('menu', './assets/Images/Menu.png');
